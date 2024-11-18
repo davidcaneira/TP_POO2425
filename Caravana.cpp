3905c7a51662fa9d9df88a0a6dae66ad92a36edb
David Caneira <david.caneira@hotmail.com>
2024-11-18 23:07:15 +0000

    caravana

--- Caravana.cpp
+++ Caravana.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Caravana.h"
+/*
+Caravana::Caravana(int id, int tripulantes, int capacidadeCarga, int capacidadeAgua, int posX, int posY)
+    : id(id), tripulantes(tripulantes), capacidadeCarga(capacidadeCarga), capacidadeAgua(capacidadeAgua),
+      aguaAtual(capacidadeAgua), cargaAtual(0), posX(posX), posY(posY), modoAutonomo(false) {}
+
+void Caravana::mover(char direcao) {
+    switch (direcao) {
+    case 'D': posX++; break; // Direita
+    case 'E': posX--; break; // Esquerda
+    case 'C': posY--; break; // Cima
+    case 'B': posY++; break; // Baixo
+    case 'CE': posX--; posY--; break; // Cima-Esquerda
+    case 'CD': posX++; posY--; break; // Cima-Direita
+    case 'BE': posX--; posY++; break; // Baixo-Esquerda
+    case 'BD': posX++; posY++; break; // Baixo-Direita
+    default:
+        std::cout << "Direção inválida!\n";
+    }
+}
+
+void Caravana::exibirInfo() const {
+    std::cout << "Caravana ID: " << id << "\n"
+              << "Tipo: " << getTipo() << "\n"
+              << "Tripulantes: " << tripulantes << "\n"
+              << "Carga Atual: " << cargaAtual << " toneladas\n"
+              << "Água Atual: " << aguaAtual << " litros\n"
+              << "Posição: (" << posX << ", " << posY << ")\n";
+}
+
+void Caravana::atualizarEstado() {
+    if (aguaAtual > 0) {
+        aguaAtual -= tripulantes; // Consome água com base no número de tripulantes
+    } else if (tripulantes > 0) {
+        tripulantes--; // Perde tripulantes se ficar sem água
+    }
+}
+
+int Caravana::getId() const { return id; }
+int Caravana::getTripulantes() const { return tripulantes; }
+void Caravana::setTripulantes(int valor) { tripulantes = valor; }
+int Caravana::getAguaAtual() const { return aguaAtual; }
+void Caravana::setAguaAtual(int valor) { aguaAtual = valor; }
+int Caravana::getCargaAtual() const { return cargaAtual; }
+void Caravana::setCargaAtual(int valor) { cargaAtual = valor; }
+
+*/
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/anton/Desktop/universidade/Poo/TP_POO2425
+# Install script for directory: C:/Users/David/Desktop/POO/TP_POO2425
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/anton/Desktop/universidade/Poo/TP_POO2425/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/David/Desktop/POO/TP_POO2425/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
